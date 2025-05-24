5749ddf888a25e4d4b29a6d44cb35c6196ac0b79
Cotton Hou <himcotton@gmail.com>
2025-05-25 00:56:36 +0800

    add main

---
+++ main.mjs
@@ -0,0 +1,15 @@
+import { Readable } from 'node:stream';
+import { createInterface } from 'node:readline';
+
+const input = Readable.from(`
+    l1
+    l2
+    l3
+`);
+
+const stream = createInterface({ input });
+
+const lines = await Array.fromAsync(stream, str => str.trim());
+
+console.log(lines.filter(Boolean));
+
